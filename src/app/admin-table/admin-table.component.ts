91a2ad5bd5810bcf30da2ca72e9f12cff34e2efc
Sid1772 <sidbhatia60@gmail.com>
2021-08-27 13:42:59 +0530

    Commit 2

--- src/app/admin-table/admin-table.component.ts
+++ src/app/admin-table/admin-table.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { AdminService } from '../admin.service';
 import { Members } from './admin-grid-type';
 import { MatTableDataSource } from '@angular/material/table';
@@ -9,18 +9,18 @@ import { MatPaginator } from '@angular/material/paginator';
   templateUrl: './admin-table.component.html',
   styleUrls: ['./admin-table.component.css'],
 })
-export class AdminTableComponent implements OnInit, AfterViewInit {
+export class AdminTableComponent implements OnInit {
   gridData!: MatTableDataSource<any>;
   tableData!: Array<any>;
-  rowsPerPage = 10;
-  noOfPages = 0;
+  rowsPerPage = 10; //Number Of Entries per Page
+  noOfPages = 0; //Number of Pages in the Table
   currentPage = 1;
-  countShown = 5;
-  roles = ['member', 'admin'];
+  countShown = 5; //Number of Pages shown in Paginator
+  roles = ['member', 'admin']; //Roles of Users
   selectedRows: Array<any> = [];
   allSelected = false;
-  @ViewChild('paginator') Paginator!: MatPaginator;
-  displayedColumns = ['check', 'name', 'email', 'role', 'action'];
+  totalRecords=0
+  displayedColumns = ['check', 'name', 'email', 'role', 'action']; //Columns to display in Table
   constructor(private service: AdminService) {}
 
   ngOnInit(): void {
@@ -30,7 +30,10 @@ export class AdminTableComponent implements OnInit, AfterViewInit {
       this.setData();
     });
   }
-  ngAfterViewInit() {}
+  /**
+   * filterGridData - Function to filter Data in Table on basis of keywords in Search bar
+   * @param filterString
+   */
   filterGridData(filterString: string) {
     filterString = filterString.trim();
     filterString = filterString.toLowerCase();
@@ -39,33 +42,70 @@ export class AdminTableComponent implements OnInit, AfterViewInit {
     this.currentPage = 1;
     this.setData();
   }
-  returnArray(length: number) {
-    return Array(length);
+
+  /**
+   * returnArray- Called from Html File to get the Number of pages to show in the paginator Max count will be countShown
+   * @returns Array<Number>
+   */
+  returnArray() {
+    var arr = [];
+    const page = this.currentPage + this.countShown;
+    if (page <= this.noOfPages) {
+      for (let i = this.currentPage; i < page; i++) {
+        arr.push(i);
+      }
+    } else {
+      var diff =this.currentPage-(page-this.noOfPages)+1;
+      if(diff<=0)diff=1
+      for (let i = diff; i <= this.noOfPages; i++) {
+        arr.push(i);}
+    }
+    return arr;
   }
+  /**
+   * Changes the current page as per selection from html file
+   * @param page
+   * @return void
+   */
   changePage(page: number) {
+    if (page > this.noOfPages) return;
     this.currentPage = page;
     this.setData();
   }
+  /**
+   * Finds total  number of pages in the grid
+   * @param length
+   */
   findNumberOfPages(length: number) {
     this.noOfPages = Math.ceil(length / 10 < 1 ? 1 : length / 10);
   }
+  /**
+   * Sets Data according to Page Number selected
+   */
   setData() {
+    this.totalRecords=this.gridData.filteredData.length
     const dataIndex = (this.currentPage - 1) * this.rowsPerPage;
     this.tableData = this.gridData.filteredData.slice(
       dataIndex,
       dataIndex + 10
     );
   }
+  /**
+   * Deletes single or multiple rows as per selection using Rest operators as params
+   * @param row
+   */
   delete(...row: any) {
-    console.log(row);
     row.forEach((element: any) => {
-      console.log(element);
       var index = this.gridData.filteredData.indexOf(element);
       this.gridData.filteredData.splice(index, 1);
     });
     this.findNumberOfPages(this.gridData.filteredData.length);
     this.setData();
   }
+  /**
+   * Adds or removes rows from selectedRows Array as per checkbox selection in UI
+   * @param row
+   */
   toggle(row: any) {
     if (this.selectedRows.includes(row)) {
       var index = this.selectedRows.indexOf(row);
@@ -79,20 +119,21 @@ export class AdminTableComponent implements OnInit, AfterViewInit {
       this.allSelected = false;
     }
   }
+  /**
+   * Called on selection of header checkbox ,Selects or deselects all checkboxes in that page
+   */
   toggleAll() {
     if (this.allSelected) {
       this.allSelected = false;
       this.selectedRows = [];
     } else {
-      const selectedIndexStart = (this.currentPage - 1) * this.rowsPerPage;
-      const selectedIndexEnd = selectedIndexStart + this.rowsPerPage;
-      this.selectedRows = this.tableData.slice(
-        selectedIndexStart,
-        selectedIndexEnd
-      );
+      this.selectedRows = this.tableData.slice(0, this.rowsPerPage);
       this.allSelected = true;
     }
   }
+  /**
+   * Delete multiple rows
+   */
   deleteMultiple() {
     this.delete(...this.selectedRows);
     this.allSelected = false;
--- src/app/admin.service.ts
+++ src/app/admin.service.ts
@@ -9,6 +9,9 @@ export class AdminService {
   constructor(private http:HttpClient) { }
 
   public getTableData():any{
+    //GeekTrust API with 46 users
     return this.http.get('https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json')
+    //Mocky APi with more users for testing
+    //return this.http.get('https://run.mocky.io/v3/a2050c68-9e97-476d-927a-9230a9d8ca05')
   }
 }
